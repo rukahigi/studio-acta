0f534ba3de4b430ea3df271b56f92821720d568b
RUKAHIGI <tabigre@gmail.com>
2026-09-11 02:21:46 +0900

    Japanese code block

--- src/scripts/code-block.ts
+++ src/scripts/code-block.ts
@@ -28,8 +28,8 @@ function enhanceBlock(pre: HTMLPreElement) {
 	const copyButton = document.createElement('button');
 	copyButton.type = 'button';
 	copyButton.className = 'code-copy-btn';
-	copyButton.title = '复制代码';
-	copyButton.setAttribute('aria-label', '复制代码');
+	copyButton.title = 'コードをコピー';
+	copyButton.setAttribute('aria-label', 'コードをコピー');
 	copyButton.innerHTML = `
 		<svg class="icon-copy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
 			<rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
